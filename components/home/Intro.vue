b41174b72fad27cc01f536ead19862b5f09f9318
davidlicen <david@artnetik.si>
2018-10-18 18:09:11 +0200

    Update intro

--- components/home/Intro.vue
+++ components/home/Intro.vue
@@ -6,12 +6,18 @@
           <h1>
             Bringing together the NFT world.
           </h1>
-          <div class="margin-bottom-3">
-            <p class="intro">
+          <div class="margin-bottom-3 intro">
+            <p>
               As diverse and unique as the non-fungible space is, so are the visions, potentials, and opinions about it.
               <strong>Our goal is to provide a common platform</strong> for all non-fungible thinkers, enthusiasts, and creators. Join in and share your part, too.
             </p>
             <p>Some of the non-fungible perks:</p>
+            <ul>
+              <li>Exposure of your project</li>
+              <li>Early access to NFT solutions</li>
+              <li>Large community and network</li>
+
+            </ul>
           </div>
           <div class="margin-bottom-3">
             <a href="#" v-scroll-to="{el: '#platform'}" class="button primary large margin-right-1 margin-bottom-1">Become a member</a>
@@ -42,12 +48,14 @@
     line-height: 1.3;
 
     @include breakpoint(medium) {
+      position: absolute;
       font-size: 3rem;
     }
   }
 
   @include breakpoint(medium) {
     .intro {
+      padding-top: 6rem;
       max-width: 550px;
       font-size: 20px
     }
