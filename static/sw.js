b8afac059d8fcf3aee760622774c9c38b3bfb171
davidlicen <david@artnetik.si>
2019-04-19 10:25:14 +0200

    Update partners and ignore file

--- static/sw.js
+++ static/sw.js
@@ -2,40 +2,32 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/12cba4d8bb141c988da4.js",
-    "revision": "e857f00770e1f90d763da8c977ac23b3"
-  },
-  {
-    "url": "/_nuxt/15350301406ab820d241.js",
-    "revision": "97e3a2e745f1f31ee432f3c614ce757c"
-  },
-  {
-    "url": "/_nuxt/1cc4c174767f536a4ed8.js",
-    "revision": "9a3ed009d73e909ca687270c4ba34add"
-  },
-  {
     "url": "/_nuxt/267bbdb3d1c307a0c4ba.js",
     "revision": "4237943c3098ee78b89e39cd2fc5473e"
+  },
+  {
+    "url": "/_nuxt/397d71321ad5380b411d.js",
+    "revision": "272c0f4c212bdf61baf97bfa037297cf"
   },
   {
     "url": "/_nuxt/41b753550d0999af598a.js",
     "revision": "3cdad35b30a670f983f38743d5487c31"
   },
   {
-    "url": "/_nuxt/5c29429a1e580b2d4350.js",
-    "revision": "25e051d3faeea133dac54043df32fc25"
-  },
-  {
     "url": "/_nuxt/6d7c4a490c3622e683af.js",
     "revision": "0ec429a88a0d07816564b817420b6413"
+  },
+  {
+    "url": "/_nuxt/6f5dfd0ba46294fefadd.js",
+    "revision": "c41ae0cbc21af563b485e71593b0f9ea"
   },
   {
     "url": "/_nuxt/712384c8fc9212384a3c.js",
     "revision": "1a3fa601682f449b0a4a23c3a7a456cb"
   },
   {
-    "url": "/_nuxt/73c5af0f7552438c3cac.js",
-    "revision": "3208a912dee0d854c84d64899c511c42"
+    "url": "/_nuxt/7c06427ce6353f9e448a.js",
+    "revision": "ffc1773d06e8c0d2664eae549cf7ab83"
   },
   {
     "url": "/_nuxt/aaa18fdcf6e4e9dfd70f.js",
@@ -50,8 +42,16 @@ workbox.precaching.precacheAndRoute([
     "revision": "f941345354b1d2825b37626007212c41"
   },
   {
-    "url": "/_nuxt/db8306e1610cd44a9f86.js",
-    "revision": "fdead6cfb9fb9ce2d00c2767ab23086b"
+    "url": "/_nuxt/e1035a827b6bfaaa679e.js",
+    "revision": "397241e33c09bc73bf6594da875a0c76"
+  },
+  {
+    "url": "/_nuxt/edde4f1dbc99489b125a.js",
+    "revision": "7d87b238837c95cebbde0378fd1f76b6"
+  },
+  {
+    "url": "/_nuxt/f8cb55eedae1d9ca1840.js",
+    "revision": "dbdb37d236bf3d8b1f26fd455590a400"
   }
 ], {
   "cacheId": "nf-alliance-www",
